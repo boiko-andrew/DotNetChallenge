edb4028439282495ccb4345f5dd1f64785d142fe
Бойко Андрей <boiko_andrew@mail.ru>
2020-01-09 23:47:40 +0300

    2nd commit. Added assignment ModularCircle.

---
+++ ModularCircle/ModularCircle/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ModularCircle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double _circleRadius;
+        private int _circlePointCount;
+        private double _pointRadius;
+
+        private int _fromIndex;
+        private int _toIndex;
+
+        private readonly Brush _mainCircleBrush = Brushes.OrangeRed;
+        private readonly Brush _pointBrush = Brushes.CornflowerBlue;
+        private readonly Brush _lineBrush = Brushes.CornflowerBlue;
+
+        private const double _factor = 51;
+
+        public MainWindow()
+        {
+            Loaded += (sender, args) => Start();
+            InitializeComponent();
+
+            _circleRadius = (canvas.Height - 20) / 2;
+            _circlePointCount = 1000;
+            _pointRadius = 2;
+        }
+
+        private void Start()
+        {
+            double marginX = (canvas.Width - 2 * _circleRadius) / 2;
+            double marginY = (canvas.Height - 2 * _circleRadius) / 2;
+
+            //рассчитываем координаты точек на окружности
+            //Point[] circlePoints = CirclePointsCalculator.CalcCirclePoints(_circlePointCount, _circleRadius, marginX + _circleRadius, marginY + _circleRadius);
+
+            //рассчитываем координаты точек на окружности с помощью
+            //самостоятельно написанной функции
+            Point[] circlePoints = MyCirclePointsCalculator.CalcCirclePoints(_circlePointCount, _circleRadius,
+                marginX + _circleRadius, marginY + _circleRadius);
+
+            //рисуем основную окружность
+            DrawCircle(_circleRadius, _mainCircleBrush, marginX, marginY);
+
+            //рисуем точки на окружности ввиде маленьких кружочков
+            foreach (var point in circlePoints)
+            {
+                DrawCircle(_pointRadius, _pointBrush, point.X - _pointRadius, point.Y - _pointRadius, true);
+            }
+
+            //todo: из каждой точки на окружности провести линию, используя метод DrawLine
+            //Номер точки, в которую нужно вести линию вычисляется по формуле из видео
+
+            for (int i = 1; i < _circlePointCount; ++i)
+            {
+                _fromIndex = i;
+                _toIndex = (int)((i * _factor) % _circlePointCount);
+                DrawLine(circlePoints[_fromIndex], circlePoints[_toIndex], _lineBrush);
+            }
+
+        }
+
+        private void DrawLine(Point from, Point to, Brush brush)
+        {
+            Line line = new Line()
+            {
+                X1 = from.X,
+                Y1 = from.Y,
+                X2 = to.X,
+                Y2 = to.Y,
+                Stroke = brush,
+                StrokeThickness = 1
+            };
+
+            canvas.Children.Add(line);
+        }
+
+        private void DrawCircle(double radius, Brush brush, double x, double y, bool fill = false)
+        {
+            var circle = new Ellipse()
+            {
+                Stroke = brush,
+                Width = 2 * radius,
+                Height = 2 * radius,
+                StrokeThickness = 1
+            };
+
+            if (fill)
+                circle.Fill = brush;
+
+            Canvas.SetLeft(circle, x);
+            Canvas.SetTop(circle, y);
+
+            canvas.Children.Add(circle);
+        }
+
+
+    }
+}
---
+++ ModularCircle/ModularCircle/MyCirclePointsCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace ModularCircle
+{
+    class MyCirclePointsCalculator
+    {
+        public static Point[] CalcCirclePoints(int circlePointCount, double radius, double x, double y)
+        {
+            Point[] points = new Point[circlePointCount];
+            double alpha;
+
+            for (int i = 0; i < circlePointCount; ++i)
+            {
+                alpha = circlePointCount != 0 ? 2 * Math.PI / circlePointCount * i :
+                    2 * Math.PI / 200 * i;
+                points[i].X = x + Math.Cos(alpha) * radius;
+                points[i].Y = y + Math.Sin(alpha) * radius;
+            }
+            return points;
+        }
+    }
+}
